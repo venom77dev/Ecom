ff6e26f92b0a91c4f9fdcb63b12a9c6a186e652a
olivia <manali06@proton.me>
2026-02-16 12:41:36 +0530

    code upload

---
+++ platform/plugins/paystack/src/Providers/PGRedirectRequestRes.php
@@ -0,0 +1,13 @@
+<?php
+namespace Botble\Paystack\Providers;
+
+class PGRequestRes
+{
+
+    public $status = false;
+    public $checkout_url = null;
+    public $token = null;
+    public $extTransactionId = null;
+    public $amount = null;
+}
+
