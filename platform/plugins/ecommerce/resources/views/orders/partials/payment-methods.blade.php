a1e4b99fa414ffade3df65aaed7daa338354f297
olivia <manali06@proton.me>
2026-05-12 10:36:30 +0530

    payment condition update

--- platform/plugins/ecommerce/resources/views/orders/partials/payment-methods.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/partials/payment-methods.blade.php
@@ -21,7 +21,6 @@
             <p> '.$item->description.'</p>
                 </div>
                 <div class="payment-method-logo">
-                <img src="/custom/'.$item->slug.'.png" loading="lazy">
             </div>
         </li>
             ';
